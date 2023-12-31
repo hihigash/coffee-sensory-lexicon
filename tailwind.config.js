75f9000a5d3daee2cacab94ca6649c23c7a831f1
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2023-12-31 15:47:51 +0900

    Add PostCSS and Tailwind CSS configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      jptext: ["Noto Sans JP"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
